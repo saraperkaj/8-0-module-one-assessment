46f87848f4eb46158b50ea09ea1d66d1045bb120
Sara Perkaj <saraperkaj@pursuit.org>
2021-08-13 15:10:48 -0400

    saving progress, just in case.

--- index.js
+++ index.js
@@ -258,7 +258,30 @@ function getAllMoviesReleasedAtOrBeforeYear(movies, year) {
  *  getBiggestBoxOfficeMovie(movies);
  *  //> "Incredibles 2"
  */
-function getBiggestBoxOfficeMovie(movies) {}
+function getBiggestBoxOfficeMovie(movies) {
+  //declare a var named current and set it to 0
+  let current = 0;
+  //if there are no movies
+  if (movies.length === 0) {
+    //return null
+    return null;
+  }
+  //for of loop through movies
+  for (const movie of movies) {
+    //make the box office a number
+    Number(movie.boxOffice);
+    //if the box office amount is more than the current
+    if (movie.boxOffice > current) {
+      //then the current is now the box office amount bc its the highest
+      current = boxOffice;
+    }
+  }
+  //outside of the loop if its the current
+  if (current) {
+    //then return the name of the movie
+    return movie.title;
+  }
+}
 
 // Do not change anything below this line.
 module.exports = {
